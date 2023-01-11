eddb8915619cd3a10af17c240158d0f8a34733ff
D97 <datnt3009@gmail.com>
2023-01-12 06:09:17 +0700

    Fixed unit test :)

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -8,8 +8,7 @@ public class ValuesController : ControllerBase
 	[HttpGet]
 	public ActionResult<IEnumerable<string>> Get()
 	{
-		return new List<string>();
-		//{ "value 1", "value 2" };
+		return new List<string> { "value 1", "value 2" };
 	}
 
 	[HttpGet("{id}")]
@@ -21,12 +20,12 @@ public class ValuesController : ControllerBase
 	[HttpPost]
 	public ActionResult<IEnumerable<string>> Post([FromBody] string value)
 	{
-		List<string> values = new List<string>();
-		//{
-		//	"value 1",
-		//	"value 2",
-		//	value
-		//};
+		List<string> values = new List<string>
+		{
+			"value 1",
+			"value 2",
+			value
+		};
 
 		return values;
 	}
